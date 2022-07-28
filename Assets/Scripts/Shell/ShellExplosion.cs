416a8aa44c6227647fccbf9366865fbf6d0fd276
Jay Warner <jaywebwar@gmail.com>
2022-07-27 22:40:41 -0400

    Uploading completed project

---
+++ Assets/Scripts/Shell/ShellExplosion.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+
+public class ShellExplosion : MonoBehaviour
+{
+    public LayerMask m_TankMask;
+    public ParticleSystem m_ExplosionParticles;       
+    public AudioSource m_ExplosionAudio;              
+    public float m_MaxDamage = 100f;                  
+    public float m_ExplosionForce = 1000f;            
+    public float m_MaxLifeTime = 2f;                  
+    public float m_ExplosionRadius = 5f;              
+
+
+    private void Start()
+    {
+        Destroy(gameObject, m_MaxLifeTime);
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Find all the tanks in an area around the shell and damage them.
+        Collider[] colliders = Physics.OverlapSphere(transform.position, m_ExplosionRadius, m_TankMask);
+
+        for(int i = 0; i < colliders.Length; i++)
+        {
+            Rigidbody targetRigidbody = colliders[i].GetComponent<Rigidbody>();
+            if(!targetRigidbody)
+                continue;
+
+            targetRigidbody.AddExplosionForce(m_ExplosionForce, transform.position, m_ExplosionRadius);
+
+            TankHealth targetHealth = targetRigidbody.GetComponent<TankHealth>();
+            if(!targetHealth)
+                continue;
+            
+            float damage = CalculateDamage(targetRigidbody.position);
+
+            targetHealth.TakeDamage(damage);
+
+        }
+        // Leave the Explosion particle system at the point of impact by unparenting from shell gameobject.
+        m_ExplosionParticles.transform.parent = null;
+
+        m_ExplosionParticles.Play();
+        m_ExplosionAudio.Play();
+
+        // Destroy the particles at the end of their duration and destroy this gameobject
+        Destroy(m_ExplosionParticles.gameObject, m_ExplosionParticles.main.duration);
+        Destroy(gameObject);
+    }
+
+
+    private float CalculateDamage(Vector3 targetPosition)
+    {
+        // Calculate the amount of damage a target should take based on it's position.
+
+        Vector3 explosionCenterToTarget = targetPosition - transform.position;
+        float targetDistanceFromExplosion = explosionCenterToTarget.magnitude;
+
+        // normalize the distance to the radius of the explosion.
+        float normalDistance = (m_ExplosionRadius - targetDistanceFromExplosion)/m_ExplosionRadius;
+
+        float damage = normalDistance * m_MaxDamage;
+
+        // Enforces that one cannot receive negative damage in the event where the center transform of
+        // the targeted gameObject is outside of the explosion radius, but the collider is within.
+        damage = Mathf.Max(0f, damage);
+        
+        return damage;
+    }
+}
---
+++ Assets/Scripts/Tank/TankShooting.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TankShooting : MonoBehaviour
+{
+    public int m_PlayerNumber = 1;       
+    public Rigidbody m_Shell;            
+    public Transform m_FireTransform;    
+    public Slider m_AimSlider;           
+    public AudioSource m_ShootingAudio;  
+    public AudioClip m_ChargingClip;     
+    public AudioClip m_FireClip;         
+    public float m_MinLaunchForce = 15f; 
+    public float m_MaxLaunchForce = 30f; 
+    public float m_MaxChargeTime = 0.75f;
+
+    private string m_FireButton;         
+    private float m_CurrentLaunchForce;  
+    private float m_ChargeSpeed;         
+    private bool m_Fired;                
+
+
+    private void OnEnable()
+    {
+        m_CurrentLaunchForce = m_MinLaunchForce;
+        m_AimSlider.value = m_MinLaunchForce;
+    }
+
+
+    private void Start()
+    {
+        m_FireButton = "Fire" + m_PlayerNumber;
+
+        m_ChargeSpeed = (m_MaxLaunchForce - m_MinLaunchForce) / m_MaxChargeTime;
+    }
+
+    // Track the current state of the fire button and make decisions based on the current launch force.
+    private void Update()
+    {
+        m_AimSlider.value = m_MinLaunchForce;
+        if(m_CurrentLaunchForce >= m_MaxLaunchForce && !m_Fired)
+        {
+            // Charged shot reaches max charge
+            m_CurrentLaunchForce = m_MaxLaunchForce;
+            Fire();
+        }
+        else if(Input.GetButtonDown(m_FireButton))
+        {
+            //player starts charging start
+            m_Fired = false;
+            m_CurrentLaunchForce = m_MinLaunchForce;
+
+            m_ShootingAudio.clip = m_ChargingClip;
+            m_ShootingAudio.Play();
+        }
+        else if (Input.GetButton(m_FireButton) && !m_Fired)
+        {
+            //player is charging shot
+            m_CurrentLaunchForce += m_ChargeSpeed * Time.deltaTime;
+
+            m_AimSlider.value = m_CurrentLaunchForce;
+        }
+        else if(Input.GetButtonUp(m_FireButton) && !m_Fired)
+        {
+            Fire();
+        }
+
+    }
+
+    // Instantiate and launch the shell.
+    private void Fire()
+    {
+        // Flag called right away.
+        m_Fired = true;
+
+        // Create shell and set velocity.
+        Rigidbody shellRb = Instantiate(m_Shell, m_FireTransform.position, m_FireTransform.rotation) as Rigidbody;
+        shellRb.velocity = m_CurrentLaunchForce * m_FireTransform.forward;
+
+        // Change and play audio clip.
+        m_ShootingAudio.clip = m_FireClip;
+        m_ShootingAudio.Play();
+
+        // Reset CurrentLaunchForce for next shot.
+        m_CurrentLaunchForce = m_MinLaunchForce;
+    }
+}
